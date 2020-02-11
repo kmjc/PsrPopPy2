477412c6b0aeef37d4a8642f1a09d3ffc2cb144d
NihanPol <nihan1593@gmail.com>
2020-02-11 13:47:11 -0500

    add SNR of only detected pulsar to their instance

--- lib/python/survey.py
+++ lib/python/survey.py
@@ -524,8 +524,6 @@ class Survey:
                                            4)
                 print "gamma harm4 = ", gamma
 
-        #Save the S/N to the pulsar object
-        pulsar.snr = sig_to_noise * degfac
         # return the S/N accounting for beam offset
         
         return sig_to_noise * degfac
